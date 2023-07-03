428d1de00cf6d93ff6e86cd35bccaec241bca17c
Shuozhi Zuo <89622865+sushi133@users.noreply.github.com>
2023-07-03 09:19:01 -0600

    Add files via upload

---
+++ National Job Corps Study/others/code/Addtional_Information.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(scales)
+
+#data
+data <- read.csv("/Users/sushi5824907/Desktop/Mediation/JASA/Data/Jobcorpdata.csv")
+data$x1<-as.factor(data$x1)
+data$x2<-as.factor(data$x2)
+data$x3<-as.factor(data$x3)
+data$x4<-as.factor(data$x4)
+data$x5<-as.factor(data$x5)
+data$x6<-as.factor(data$x6)
+data$x7<-as.factor(data$x7)
+#indicator for outcome I(Y>0)
+data$I_Y<-ifelse(data$Y==0,0,1)
+data$I_Y<-ifelse(is.na(data$Y),NA,data$I_Y)
+#annual to weekly 
+data$Y<-data$Y/52.1429
+
+#the percentages of subjects having the outcome Y observed among subjects with missing mediator values
+#treatment
+t_obsy<-data %>%
+  filter(R_m==0,Z==1) %>%
+  group_by(R_y) %>%
+  summarise(n = n()) %>%
+  mutate(freq = scales::percent(n/sum(n), accuracy = 0.01))
+subset(t_obsy,R_y==1)
+#control
+c_obsy<-data %>%
+  filter(R_m==0,Z==0) %>%
+  group_by(R_y) %>%
+  summarise(n = n()) %>%
+  mutate(freq = scales::percent(n/sum(n), accuracy = 0.01))
+subset(c_obsy,R_y==1)
+
+#small portions of the missingness exist in covariates X
+#missing educ: 4
+educ<-data %>%
+  group_by(x6) %>%
+  summarise(n = n()) %>%
+  mutate(freq = scales::percent(n/sum(n), accuracy = 0.01))
+subset(educ,x6==4)
+#missing earnb: 5
+earnb<-data %>%
+  group_by(x7) %>%
+  summarise(n = n()) %>%
+  mutate(freq = scales::percent(n/sum(n), accuracy = 0.01))
+subset(earnb,x7==5)
+#missing arrst: 2
+arrst<-data %>%
+  group_by(x5) %>%
+  summarise(n = n()) %>%
+  mutate(freq = scales::percent(n/sum(n), accuracy = 0.01))
+subset(arrst,x5==2)
+
+#number of obs with Y=0
+num_0<-data %>%
+  group_by(I_Y) %>%
+  summarise(n = n()) %>%
+  mutate(freq = scales::percent(n/sum(n), accuracy = 0.01))
+subset(num_0,I_Y==0)
+
+
